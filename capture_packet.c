86a2a9587ddf344f8a0fcda6f0776405580fc3eb
hanjjeee <ghfkddlrrhfl@ajou.ac.kr>
2024-03-16 14:30:41 +0900

    메소드 분리

--- capture_packet.c
+++ capture_packet.c
@@ -1,6 +1,18 @@
-#include <winsock2.h> 
+// #include <winsock2.h> 
 #include <stdio.h>
 #include <pcap.h>
+
+#define HDR 3592
+#define ETHHDR 0
+#define IPHDR 1
+#define ARPHDR 2
+#define RARPHDR 3
+#define TCPHDR 4
+#define UDPHDR 5
+
+#define IP_PROTOCOL 0x0800
+#define ARP_PROTOCOL 0x0866
+#define RARP_PROTOCOL 0x0835
 
 // Ethernet Header Structure
 struct ethhdr {
@@ -52,6 +64,52 @@ struct udphdr {
 // log
 FILE *logFile = NULL;
 
+
+// packet
+u_char *packet = NULL;
+
+// hdr
+struct pcap_pkthdr *pkthdr = NULL;
+struct ethhdr *eth_header = NULL;
+struct iphdr *ip_header = NULL;
+struct tcphdr *tcp_header = NULL;
+struct udphdr *udp_header = NULL;
+
+// declare
+void printHdr(int type);
+char* getETHHdrProtocol(short int type);
+char* getIPHdrProtocol(uint8_t ip_p);
+char* getIpFormat(uint32_t ip_address);
+
+void packet_handler(u_char *user_data, const struct pcap_pkthdr *packet_header, const u_char *packet_data) {
+
+    pkthdr = packet_header;
+    packet = packet_data;
+    eth_header = (struct ethhdr *)packet;
+    ip_header = (struct iphdr *)(packet + 14);
+    tcp_header = (struct tcphdr *)(packet + 34);
+    udp_header = (struct udphdr *)(packet + 34);
+
+    printHdr(HDR);
+    printHdr(ETHHDR);
+
+    switch(ntohs(eth_header->ptype)){
+        case IP_PROTOCOL:
+            printHdr(IPHDR);
+            break;
+        case ARP_PROTOCOL:
+            // TODO
+            break;
+        case RARP_PROTOCOL:
+            // TODO
+            break;
+    }
+
+    // printHdr(TCPHDR);
+    // printHdr(UDPHDR);
+
+}
+
 char* getIpFormat(uint32_t ip_address){
 
      static char ip_string[16];
@@ -64,6 +122,23 @@ char* getIpFormat(uint32_t ip_address){
 
     return ip_string;
 
+}
+
+char* getETHHdrProtocol(short int type) {
+    static char protocol[100];
+
+    switch(type){
+        case IP_PROTOCOL: sprintf(protocol, "%s", "IP");
+            break;
+        case ARP_PROTOCOL: sprintf(protocol, "%s", "ARP");
+            break;
+        case RARP_PROTOCOL: sprintf(protocol, "%s", "RARP");
+            break;
+        default: sprintf(protocol, "%x", type);
+            break;
+    }
+
+    return protocol;
 }
 
 char* getIPHdrProtocol(uint8_t ip_p){
@@ -129,150 +204,149 @@ char* getIPHdrProtocol(uint8_t ip_p){
     return protocol;
 }
 
-void packet_handler(u_char *user_data, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
-    // Print the entire packet (customize this part based on your needs)
-    printf("Packet Captured! Length: %d\n", pkthdr->len);
-    printf("\n=============HEADER===============\n");
-    printf("Time Stamp: %ld.%06ld\n", pkthdr->ts.tv_sec, pkthdr->ts.tv_usec);
-    printf("caplen: %u",pkthdr->caplen);
-    printf("len: %u",pkthdr->len);
-    printf("\n==================================\n");
-    for (int i = 0; i < pkthdr->len; ++i) {
-        printf("%02x ", packet[i]); // 패킷의 각 바이트를 16진수로 출력
-        if ((i + 1) % 16 == 0) //  각 라인마다 16바이트씩 줄 바꿈을 수행
-            printf("\n");
+void printHdr(int type){
+    switch(type) {
+        case HDR :
+            printf("Packet Captured! Length: %d\n", pkthdr->len);
+            printf("\n=============HEADER===============\n");
+            printf("Time Stamp: %ld.%06ld\n", pkthdr->ts.tv_sec, pkthdr->ts.tv_usec);
+            printf("caplen: %u",pkthdr->caplen);
+            printf("len: %u",pkthdr->len);
+            printf("\n==================================\n");
+            for (int i = 0; i < pkthdr->len; ++i) {
+                printf("%02x ", packet[i]); // 패킷의 각 바이트를 16진수로 출력
+                if ((i + 1) % 16 == 0) //  각 라인마다 16바이트씩 줄 바꿈을 수행
+                    printf("\n");
+            }
+            printf("\n\n");
+
+            // log
+            fprintf(logFile,"Packet Captured! Length: %d\n", pkthdr->len);
+            fprintf(logFile,"\n=============HEADER===============\n");
+            fprintf(logFile,"Time Stamp: %ld.%06ld\n", pkthdr->ts.tv_sec, pkthdr->ts.tv_usec);
+            fprintf(logFile,"caplen: %u",pkthdr->caplen);
+            fprintf(logFile,"len: %u",pkthdr->len);
+            fprintf(logFile,"\n==================================\n");
+            for (int i = 0; i < pkthdr->len; ++i) {
+                fprintf(logFile,"%02x ", packet[i]); // 패킷의 각 바이트를 16진수로 출력
+                if ((i + 1) % 16 == 0) //  각 라인마다 16바이트씩 줄 바꿈을 수행
+                    fprintf(logFile,"\n");
+            }
+            fprintf(logFile,"\n\n");
+            break;
+
+        case ETHHDR : 
+            // ETHERNET
+            printf("\n=============ETHHDR===============\n");
+            printf("ethhdr src: ");
+            for(int i =0 ; i<6 ; i++){
+                printf("%02X ", eth_header->src[i]);
+            }
+            printf("\nethhdr dst: ");
+            for(int i =0 ; i<6 ; i++){
+                printf("%02X ", eth_header->des[i]);
+            }
+            printf("\n%s\n", getETHHdrProtocol(ntohs(eth_header->ptype)));
+            printf("\n==================================\n");
+            printf("\n\n");
+
+            fprintf(logFile, "\n=============ETHHDR===============\n");
+            fprintf(logFile, "ethhdr src: ");
+            for(int i =0 ; i<6 ; i++){
+                fprintf(logFile, "%02X ", eth_header->src[i]);
+            }
+            fprintf(logFile, "\nethhdr dst: ");
+            for(int i =0 ; i<6 ; i++){
+                fprintf(logFile, "%02X ", eth_header->des[i]);
+            }
+            fprintf(logFile, "\n%s\n", getETHHdrProtocol(ntohs(eth_header->ptype)));
+            fprintf(logFile, "\n==================================\n");
+            fprintf(logFile, "\n\n");
+            break;
+            
+        case IPHDR :
+            // IP
+            printf("\n=============IPHDR===============\n");
+            printf("ip_ver: %u\n", ip_header->ip_ver_hl >> 4);
+            printf("ip_hl: %u\n", ip_header->ip_ver_hl & 0xf);
+            printf("ip_tos: %u\n", ip_header->ip_tos);
+            printf("ip_len: %u\n", ip_header->ip_len);
+            printf("ip_id: %u\n", ip_header->ip_id);
+            printf("ip_off: %u\n", ip_header->ip_off);
+            printf("ip_ttl: %u\n", ip_header->ip_ttl);
+            printf("ip_p: %s\n", getIPHdrProtocol(ip_header->ip_p));
+            printf("ip_sum: %u\n", ip_header->ip_sum);
+            printf("ip_src: %s\n", getIpFormat(ip_header->ip_src));
+            printf("ip_dst: %s\n", getIpFormat(ip_header->ip_dst));
+            printf("\n==================================\n");
+            printf("\n\n");
+
+            fprintf(logFile,"\n=============IPHDR===============\n");
+            fprintf(logFile,"ip_ver: %u\n", ip_header->ip_ver_hl >> 4);
+            fprintf(logFile,"ip_hl: %u\n", ip_header->ip_ver_hl & 0xf);
+            fprintf(logFile,"ip_tos: %u\n", ip_header->ip_tos);
+            fprintf(logFile,"ip_len: %u\n", ip_header->ip_len);
+            fprintf(logFile,"ip_id: %u\n", ip_header->ip_id);
+            fprintf(logFile,"ip_off: %u\n", ip_header->ip_off);
+            fprintf(logFile,"ip_ttl: %u\n", ip_header->ip_ttl);
+            fprintf(logFile,"ip_p: %s\n", getIPHdrProtocol(ip_header->ip_p));
+            fprintf(logFile,"ip_sum: %u\n", ip_header->ip_sum);
+            fprintf(logFile,"ip_src: %s\n", getIpFormat(ip_header->ip_src));
+            fprintf(logFile,"ip_dst: %s\n", getIpFormat(ip_header->ip_dst));
+            fprintf(logFile,"\n==================================\n");
+            fprintf(logFile,"\n\n");
+            break;
+
+        case TCPHDR :
+            // TCP
+            printf("\n=============TCPHDR===============\n");
+            printf("th_sport: %u\n", tcp_header->th_sport);
+            printf("th_dport: %u\n", tcp_header->th_dport);
+            printf("th_seq: %u\n", tcp_header->th_seq);
+            printf("th_ack: %u\n", tcp_header->th_ack);
+            printf("th_offx2: %u\n", tcp_header->th_offx2);
+            printf("th_flags: %u\n", tcp_header->th_flags);
+            printf("th_win: %u\n", tcp_header->th_win);
+            printf("th_sum: %u\n", tcp_header->th_sum);
+            printf("th_urp: %u\n", tcp_header->th_urp);
+            printf("\n==================================\n");
+            printf("\n\n");
+
+            fprintf(logFile,"\n=============TCPHDR===============\n");
+            fprintf(logFile,"th_sport: %u\n", tcp_header->th_sport);
+            fprintf(logFile,"th_dport: %u\n", tcp_header->th_dport);
+            fprintf(logFile,"th_seq: %u\n", tcp_header->th_seq);
+            fprintf(logFile,"th_ack: %u\n", tcp_header->th_ack);
+            fprintf(logFile,"th_offx2: %u\n", tcp_header->th_offx2);
+            fprintf(logFile,"th_flags: %u\n", tcp_header->th_flags);
+            fprintf(logFile,"th_win: %u\n", tcp_header->th_win);
+            fprintf(logFile,"th_sum: %u\n", tcp_header->th_sum);
+            fprintf(logFile,"th_urp: %u\n", tcp_header->th_urp);
+            fprintf(logFile,"\n==================================\n");
+            fprintf(logFile,"\n\n");
+            break;
+
+        case UDPHDR :
+            // UDP 
+            printf("\n=============UDPHDR===============\n");
+            printf("uh_sport: %u\n", udp_header->uh_sport);
+            printf("uh_dport: %u\n", udp_header->uh_dport);
+            printf("uh_len: %u\n", udp_header->uh_len);
+            printf("uh_sum: %u\n", udp_header->uh_sum);
+            printf("\n==================================\n");
+            printf("\n\n");
+
+            fprintf(logFile,"\n=============UDPHDR===============\n");
+            fprintf(logFile,"uh_sport: %u\n", udp_header->uh_sport);
+            fprintf(logFile,"uh_dport: %u\n", udp_header->uh_dport);
+            fprintf(logFile,"uh_len: %u\n", udp_header->uh_len);
+            fprintf(logFile,"uh_sum: %u\n", udp_header->uh_sum);
+            fprintf(logFile,"\n==================================\n");
+            fprintf(logFile,"\n\n");
+            break;
+        
+        default: break;
     }
-    printf("\n\n");
-
-    // log
-    fprintf(logFile,"Packet Captured! Length: %d\n", pkthdr->len);
-    fprintf(logFile,"\n=============HEADER===============\n");
-    fprintf(logFile,"Time Stamp: %ld.%06ld\n", pkthdr->ts.tv_sec, pkthdr->ts.tv_usec);
-    fprintf(logFile,"caplen: %u",pkthdr->caplen);
-    fprintf(logFile,"len: %u",pkthdr->len);
-    fprintf(logFile,"\n==================================\n");
-    for (int i = 0; i < pkthdr->len; ++i) {
-        fprintf(logFile,"%02x ", packet[i]); // 패킷의 각 바이트를 16진수로 출력
-        if ((i + 1) % 16 == 0) //  각 라인마다 16바이트씩 줄 바꿈을 수행
-            fprintf(logFile,"\n");
-    }
-    fprintf(logFile,"\n\n");
-
-    struct ethhdr *eth_header = (struct ethhdr *)packet;
-    struct iphdr *ip_header = (struct iphdr *)(packet + 14);
-    struct tcphdr *tcp_header = (struct tcphdr *)(packet + 34);
-    struct udphdr *udp_header = (struct udphdr *)(packet + 34);
-    
-
-
-    // ETHERNET
-    printf("\n=============ETHHDR===============\n");
-    printf("ethhdr src: ");
-    for(int i =0 ; i<6 ; i++){
-        printf("%02X ", eth_header->src[i]);
-    }
-    printf("\nethhdr dst: ");
-    for(int i =0 ; i<6 ; i++){
-        printf("%02X ", eth_header->des[i]);
-    }
-    printf("\n%d\n", ntohs(eth_header->ptype));
-    printf("\n==================================\n");
-    printf("\n\n");
-
-    fprintf(logFile, "\n=============ETHHDR===============\n");
-    fprintf(logFile, "ethhdr src: ");
-    for(int i =0 ; i<6 ; i++){
-        fprintf(logFile, "%02X ", eth_header->src[i]);
-    }
-    fprintf(logFile, "\nethhdr dst: ");
-    for(int i =0 ; i<6 ; i++){
-        fprintf(logFile, "%02X ", eth_header->des[i]);
-    }
-    fprintf(logFile, "\n%d\n", ntohs(eth_header->ptype));
-    fprintf(logFile, "\n==================================\n");
-    fprintf(logFile, "\n\n");
-
-    
-
-
-    // IP
-    printf("\n=============IPHDR===============\n");
-    printf("ip_ver: %u\n", ip_header->ip_ver_hl >> 4);
-    printf("ip_hl: %u\n", ip_header->ip_ver_hl & 0xf);
-    printf("ip_tos: %u\n", ip_header->ip_tos);
-    printf("ip_len: %u\n", ip_header->ip_len);
-    printf("ip_id: %u\n", ip_header->ip_id);
-    printf("ip_off: %u\n", ip_header->ip_off);
-    printf("ip_ttl: %u\n", ip_header->ip_ttl);
-    printf("ip_p: %s\n", getIPHdrProtocol(ip_header->ip_p));
-    printf("ip_sum: %u\n", ip_header->ip_sum);
-    printf("ip_src: %s\n", getIpFormat(ip_header->ip_src));
-    printf("ip_dst: %s\n", getIpFormat(ip_header->ip_dst));
-    printf("\n==================================\n");
-    printf("\n\n");
-
-    fprintf(logFile,"\n=============IPHDR===============\n");
-    fprintf(logFile,"ip_ver: %u\n", ip_header->ip_ver_hl >> 4);
-    fprintf(logFile,"ip_hl: %u\n", ip_header->ip_ver_hl & 0xf);
-    fprintf(logFile,"ip_tos: %u\n", ip_header->ip_tos);
-    fprintf(logFile,"ip_len: %u\n", ip_header->ip_len);
-    fprintf(logFile,"ip_id: %u\n", ip_header->ip_id);
-    fprintf(logFile,"ip_off: %u\n", ip_header->ip_off);
-    fprintf(logFile,"ip_ttl: %u\n", ip_header->ip_ttl);
-    fprintf(logFile,"ip_p: %s\n", getIPHdrProtocol(ip_header->ip_p));
-    fprintf(logFile,"ip_sum: %u\n", ip_header->ip_sum);
-    fprintf(logFile,"ip_src: %s\n", getIpFormat(ip_header->ip_src));
-    fprintf(logFile,"ip_dst: %s\n", getIpFormat(ip_header->ip_dst));
-    fprintf(logFile,"\n==================================\n");
-    fprintf(logFile,"\n\n");
-
-
-    // TCP
-    printf("\n=============TCPHDR===============\n");
-    printf("th_sport: %u\n", tcp_header->th_sport);
-    printf("th_dport: %u\n", tcp_header->th_dport);
-    printf("th_seq: %u\n", tcp_header->th_seq);
-    printf("th_ack: %u\n", tcp_header->th_ack);
-    printf("th_offx2: %u\n", tcp_header->th_offx2);
-    printf("th_flags: %u\n", tcp_header->th_flags);
-    printf("th_win: %u\n", tcp_header->th_win);
-    printf("th_sum: %u\n", tcp_header->th_sum);
-    printf("th_urp: %u\n", tcp_header->th_urp);
-    printf("\n==================================\n");
-    printf("\n\n");
-
-    fprintf(logFile,"\n=============TCPHDR===============\n");
-    fprintf(logFile,"th_sport: %u\n", tcp_header->th_sport);
-    fprintf(logFile,"th_dport: %u\n", tcp_header->th_dport);
-    fprintf(logFile,"th_seq: %u\n", tcp_header->th_seq);
-    fprintf(logFile,"th_ack: %u\n", tcp_header->th_ack);
-    fprintf(logFile,"th_offx2: %u\n", tcp_header->th_offx2);
-    fprintf(logFile,"th_flags: %u\n", tcp_header->th_flags);
-    fprintf(logFile,"th_win: %u\n", tcp_header->th_win);
-    fprintf(logFile,"th_sum: %u\n", tcp_header->th_sum);
-    fprintf(logFile,"th_urp: %u\n", tcp_header->th_urp);
-    fprintf(logFile,"\n==================================\n");
-    fprintf(logFile,"\n\n");
-
-
-    // UDP 
-    // printf("\n=============UDPHDR===============\n");
-    // printf("uh_sport: %u\n", udp_header->uh_sport);
-    // printf("uh_dport: %u\n", udp_header->uh_dport);
-    // printf("uh_len: %u\n", udp_header->uh_len);
-    // printf("uh_sum: %u\n", udp_header->uh_sum);
-    // printf("\n==================================\n");
-    // printf("\n\n");
-
-    // fprintf(logFile,"\n=============UDPHDR===============\n");
-    // fprintf(logFile,"uh_sport: %u\n", udp_header->uh_sport);
-    // fprintf(logFile,"uh_dport: %u\n", udp_header->uh_dport);
-    // fprintf(logFile,"uh_len: %u\n", udp_header->uh_len);
-    // fprintf(logFile,"uh_sum: %u\n", udp_header->uh_sum);
-    // fprintf(logFile,"\n==================================\n");
-    // fprintf(logFile,"\n\n");
-    
-
 }
 
 int main() {
